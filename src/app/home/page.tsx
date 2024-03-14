c152bee79a3ec16234494ec151e0175efbb83a03
= <=>
2024-03-15 00:26:33 +0530

    Initial page with barebone structure

---
+++ src/app/home/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React from 'react';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const HomePage = () => {
+
+
+  return (
+
+    <div>
+      <div className="stars" />
+      <div className="twinkling" />
+    </div>
+  )
+}
+
+export default HomePage;
