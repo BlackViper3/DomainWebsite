7b112db20cd1c5c9595d903f51472db8adee581d
Yagzan.Manju <yagzzan@gmail.com>
2024-03-16 15:26:13 +0530

    feat : Adding additional tools in icons

--- src/app/about/SkillsComponent.tsx
+++ src/app/about/SkillsComponent.tsx
@@ -11,75 +11,63 @@ import {
   DiDjango,
   DiAndroid,
   DiMysql,
+  DiPostgresql,
+  DiJqueryLogo,
+  DiRedis,
 } from "react-icons/di";
 import {
   SiC,
   SiSpring,
   SiExpress,
+  SiNextdotjs,
+  SiFlutter,
 } from "react-icons/si";
-import techIconStyles from '@styles/techiconstyles.module.css';
+import techIconStyles from "@styles/techiconstyles.module.css";
 import { IconType } from "react-icons";
 
-const IconComponent:React.FC<{icon:IconType,dataTip:string}> = (props) =>{
-  return (<div className="tooltip tooltip-bottom" data-tip={props.dataTip}>
-  <div className={` ${techIconStyles.techIcons} `}>
-  <props.icon />
+const IconComponent: React.FC<{ icon: IconType; key: string }> = (
+  props
+) => {
+  return (
+    <div className="tooltip tooltip-bottom"  data-tip={props.key}>
+      <div className={` ${techIconStyles.techIcons} `}>
+        <props.icon />
+      </div>
     </div>
-    </div>);
-}
+  );
+};
 
-const SkillsComponent:React.FC<{className?: string}>= () => {
-
-  [DiJava].map(icon => <IconComponent  icon={icon} dataTip={icon.name} />);
+const SkillsComponent: React.FC<{ className?: string }> = () => {
+  [DiJava].map((icon) => <IconComponent  icon={icon} key={icon.name} />);
 
   return (
     <div className="grid place-content-center">
-      <div className={`grid grid-cols-2 md:grid-cols-4 ${techIconStyles.techIconsContainer}`}>              
-        <div className="tooltip tooltip-bottom" data-tip="Java">
-        <div className={` ${techIconStyles.techIcons} `}>
-          <DiJava />
-        </div>
-      
-        </div>
-        {[DiJava].map(icon => <IconComponent  icon={icon} dataTip={icon.name.replace("Di","")} />)}
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <DiJavascript1 />
-        </div>
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <DiPython />
-        </div>
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <SiC />
-        </div>
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <CgCPlusPlus />
-        </div>
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <SiSpring />
-        </div>
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <DiDjango />
-        </div>
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <DiReact />
-        </div>
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <DiAndroid />
-        </div>
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <DiNodejs />
-        </div>
-
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <DiMysql />
-        </div>
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <DiMongodb />
-        </div>
-
-        <div className = {` ${techIconStyles.techIcons} tooltip tooltip-bottom`} data-tip="Java">
-          <SiExpress />
-        </div>
+      <div
+        className={`grid grid-cols-2 md:grid-cols-4 ${techIconStyles.techIconsContainer}`}
+      >
+        {[
+          DiJava,
+          DiJavascript1,
+          DiPython,
+          SiC,
+          SiSpring,
+          DiDjango,
+          DiReact,
+          DiJqueryLogo,
+          SiNextdotjs,
+          DiNodejs,
+          DiAndroid,
+          SiFlutter,
+          DiMongodb,
+          DiMysql,
+          DiPostgresql,
+          DiRedis
+        ].map((icon) => (
+          <IconComponent
+            icon={icon}
+            key={icon.name.replace(/Di|Si/g, "")}
+          />
+        ))}
       </div>
     </div>
   );
